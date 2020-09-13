20dbb3685fc9c9d53b2f891ab680273d37e319c7
Artur Gunka <arturel101010@mail.ru>
2020-09-13 18:51:43 +0300

    Updates

---
+++ precache-manifest.22fbf04ce6732281cf6212359533a911.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bcfa8d3718bf4e02ddcbcc1bad4f5e7",
+    "url": "/nasa-picture/index.html"
+  },
+  {
+    "revision": "de56cc8a8a51d376a119",
+    "url": "/nasa-picture/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "57f50df09620011e2609",
+    "url": "/nasa-picture/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "de56cc8a8a51d376a119",
+    "url": "/nasa-picture/static/js/2.49bd50ef.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/nasa-picture/static/js/2.49bd50ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f50df09620011e2609",
+    "url": "/nasa-picture/static/js/main.6d6ca675.chunk.js"
+  },
+  {
+    "revision": "b18e556e3b00d7c7a65f",
+    "url": "/nasa-picture/static/js/runtime-main.40a7bc40.js"
+  }
+]);
