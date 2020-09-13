3b174c69e1524a451087b3393fb0eeffd3e347f0
Artur Gunka <arturel101010@mail.ru>
2020-09-13 18:39:37 +0300

    Ready

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Layout, Typography } from "antd";
+import Info from "./info";
 
 function App() {
-  return <div>Hello</div>;
+  const { Header, Footer, Content } = Layout;
+
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Header style={{ display: "flex", justifyContent: "center" }}>
+        <Typography.Title style={{ color: "white" }}>NASA API</Typography.Title>
+      </Header>
+      <Content style={{ width: "1440px", margin: "0 auto" }}>
+        <Info />
+      </Content>
+      <Footer style={{ display: "flex", justifyContent: "center" }}>
+        <Typography.Text>Developer: Artur Gunka 2020</Typography.Text>
+      </Footer>
+    </Layout>
+  );
 }
 
 export default App;
